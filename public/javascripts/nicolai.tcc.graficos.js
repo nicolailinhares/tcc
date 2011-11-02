7081d7917f4980a37e3e91972d71d3e6e5c6f69a
Nicolai Linhares <nicolai.linhares@gmail.com>
2011-11-02 15:31:45 -0200

    Inicio dos graficos

---
+++ public/javascripts/nicolai.tcc.graficos.js
@@ -0,0 +1,67 @@
+/*formato do objeto de configuracao 
+   * Configuracao = {
+   * 	eixo: [],
+   * 	tituloGrafico: String,
+   * 	tituloEixo: String,
+   * 	id: String,
+   * 	width: Number,
+   * 	height: Number
+   * }
+   * 
+*/
+configuracao = {
+	eixo: [],
+	tituloGrafico: '',
+	tituloEixo: 'Mês',
+	id: 'contemGrafico',
+	width: 600,
+	height: 400
+}
+
+function graficoBarras(raw_data, configuracao) {
+  //formato do raw_data [[String,numero,numero,numero],...]
+  //formato do configuracao.eixo: [String,String,String], -> um elemento para cada número nos arrays do raw_data
+  var data = new google.visualization.DataTable();
+  eixo = configuracao.eixo
+  data.addColumn('string', configuracao.tituloEixo);
+  for (var i = 0; i  < raw_data.length; ++i) {
+    data.addColumn('number', raw_data[i][0]);    
+  }
+  
+  data.addRows(eixo.length);
+
+  for (var j = 0; j < eixo.length; ++j) {    
+    data.setValue(j, 0, eixo[j].toString());    
+  }
+  for (var i = 0; i  < raw_data.length; ++i) {
+    for (var j = 1; j  < raw_data[i].length; ++j) {
+      data.setValue(j-1, i+1, raw_data[i][j]);    
+    }
+  }
+  
+  // Create and draw the visualization.
+  new google.visualization.ColumnChart(document.getElementById(configura.id)).
+      draw(data,
+           {title:configuracao.tituloGrafico, 
+            width:configuracao.width, height:configuracao.height,
+            hAxis: {title: configuracao.tituloEixo}}
+      );
+}
+
+function graficoPizza(raw_data,configuracao) {
+  // formato raw_data [[String,Number],...]
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', configuracao.tituloEixo);
+  data.addColumn('number', 'Quantidade');
+  lim = raw_data.length
+  data.addRows(lim);
+  for(var i = 0; i < lim; i++){
+  	data.setValue(i,0,raw_data[i][0]);
+  	data.setValue(i,1,raw_data[i][1]);
+  }
+  
+  // Create and draw the visualization.
+  new google.visualization.PieChart(document.getElementById(configuracao.id)).
+      draw(data, {title:configuracao.tituloGrafico});
+}
+
